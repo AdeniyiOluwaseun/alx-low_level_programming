8a5351d99bc9608d72c8de83d1f7be2b24595337
seun_adeniyi <seunadeniyi06@gmail.com>
2022-09-09 01:52:32 -0700

    still on the space

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
-/*the is the entrance to the code: main()*/
-int main(void)
+/* the is the entrance to the code: main() */
+int main (void)
 {
-    /*prints out string like printf also*/
-
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+	/* prints out string like printf also */
+	puts ("\"Programming is like building a multilingual puzzle"); 
+	return (0);
 }
