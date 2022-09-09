c1d1a56d826d7c2dba78fa2ac65d3eed89351d82
seun_adeniyi <seunadeniyi06@gmail.com>
2022-09-09 02:30:59 -0700

    added description

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- *main : main function
+ *main - main block
+ *Description: "Your program should end with the value 0"
  *Return: 0
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle"); 
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
