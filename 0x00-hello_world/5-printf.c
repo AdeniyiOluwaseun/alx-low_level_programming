8b081c3256e45f5d7bafd887734eb5a187d2749d
seun_adeniyi <seunadeniyi06@gmail.com>
2022-09-09 02:52:26 -0700

    removed extraneous lwords

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,12 +2,12 @@
 /**
  * main - main block
  * you are not allowed to use the function puts
- * Main : your progtam should compile without warning when using the "-Wall gcc" option
+ * Main : your program should "-Wall gcc" option
  * Description: "your program should return 0"
  * Return: always 0 (Success)
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
