13dbade7733c023454e46c125d9b99f9a45c469d
seun_adeniyi <seunadeniyi06@gmail.com>
2022-09-09 02:47:34 -0700

    add description

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
-
+/**
+ * main - main block
+ * you are not allowed to use the function puts
+ * Main : your progtam should compile without warning when using the "-Wall gcc" option
+ * Description: "your program should return 0"
+ * Return: always 0 (Success)
+ */
 int main(void)
 {
-    printf("with proper grammar, but the outcome is a piece of art,");
-    return(0);
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
 }
