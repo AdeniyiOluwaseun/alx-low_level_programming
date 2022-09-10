32f687be2df24b5e3b99a4ba92f0b01c00857356
seun_adeniyi <seunadeniyi06@gmail.com>
2022-09-10 06:52:08 -0700

    initial commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/**
+ * main - main function
+ * Description: prints out the alphabets in lower case
+ * Return: returns 0 successfully
+ */
+
+int main(void)
+{
+	char n = 'a';
+
+	while (n <= 'z' && n != 'e' && n != 'q')
+	{
+		if (n != 'e' && n != 'q')
+		{
+			putchar(n);
+		}
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
