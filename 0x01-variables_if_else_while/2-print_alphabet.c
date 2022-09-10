74d60d286488a53317bb33a6be80d797b9a1ff98
seun_adeniyi <seunadeniyi06@gmail.com>
2022-09-10 06:11:13 -0700

    initial comit for 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/**
+ * main - main function
+ * Description: prints out the alphabets in lower case
+ * Return: returns 0 successfully
+ */
+
+int main(void)
+{
+	char n = 'a';
+
+	while (n <= 'z')
+	{
+		putchar(n);
+		n++;
+	}
+	return (0);
+}
