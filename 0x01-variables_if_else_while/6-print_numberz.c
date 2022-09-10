b6ff60c8ea70a111662bbd6284f4354175bd9628
seun_adeniyi <seunadeniyi06@gmail.com>
2022-09-10 07:23:26 -0700

    initial commit for 6 prints numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/**
+ * main - main function
+ * Description: prints out the alphabets in lower case
+ * Return: returns 0 successfully
+ */
+
+int main(void)
+{
+	char n = 48;
+
+	while (n < 58)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
