c83d1542528dbf077fe9824f9c95f2d68886f8a7
seun_adeniyi <seunadeniyi06@gmail.com>
2022-09-13 06:34:46 -0700

    initial putchar commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *  main - function that begins
+ *  Return: 0 successfully
+ *  Description: using _putchar to print out all the characters
+ */
+
+int main(void)
+{
+	char mess[] = "_putchar";
+	int counter = 0;
+
+	while (counter < 9)
+	{
+	_putchar(mess[counter]);
+	counter++;
+	}
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - main function
+ * Return: 0 successfully
+ * Description: prints lower case alphabets 
+ */
+
+int main(void)
+{
+	
+}
