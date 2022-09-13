c944830549a35bd2e26cbf4c008cc8bb6c798381
seun_adeniyi <seunadeniyi06@gmail.com>
2022-09-13 08:01:08 -0700

    initial commit for 10x pprint

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet - function prints alphabets ten times
+ * Returns: no need for return funtion since the declaration returns void. 
+ */
+void print_alphabet_x10(void)
+{
+	int a = 0;
+
+	while (a < 10)
+	{
+		char x = 'a';
+
+		while (x <= 'z')
+		{
+			_putchar(x);
+			x++;
+		}
+		_putchar('\n');
+		a++;
+	}
+}
