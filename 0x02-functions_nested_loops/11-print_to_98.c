64517ada3e0e9fc4ad25e25036b19970ec33187f
seun_adeniyi <seunadeniyi06@gmail.com>
2022-09-14 08:39:13 -0700

    nest if and while statements

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,11 +8,27 @@
  */
 int print_to_98(int n)
 {
-	while (n < 98)
+	if (n <= 98)
 	{
-		printf("%d, ", n);
-		n++;
+		while (n != 98)
+		{
+			printf("%d, ", n);
+			n++;
+		}
+		printf("%d \n", 98);
 	}
-	printf("%d ", 98);
+	else if (n >= 98)
+	{
+		while (n != 98)
+		{
+			printf("%d, ", n);
+			n = n - 1;
+		}
+		printf("%d \n", 98);
+	}
+	else
+	{
+		printf("%d \n", 98);
+	}
 	return (0);
 }
