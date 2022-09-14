fb5134becc588c82775e57e84c8bd6673b7dc033
seun_adeniyi <seunadeniyi06@gmail.com>
2022-09-14 05:05:59 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_last_digit - function prints number's absolute
+ * Return: Not always 0
+ * @x: parameter is passed
+ */
+int print_last_digit(int x)
+{
+	x = x % 10;
+	_putchar(x);
+	return (0);
+}
