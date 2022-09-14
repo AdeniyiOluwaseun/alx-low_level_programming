dfa2e7258d3b56af99d7edacc3a68b05e8f31f30
seun_adeniyi <seunadeniyi06@gmail.com>
2022-09-14 02:25:54 -0700

    alpha commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _islower - function checks if character is lower case or not
+ * Return: 1 if true and 0 if false
+ * @c: parameter is passed
+ */
+int _isalpha(int c)
+{
+	return (c >= 65 && c <= 90);
+}
