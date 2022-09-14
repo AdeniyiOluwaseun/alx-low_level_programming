44ff79134f921e0b30db8e35a08ff276b595e46c
seun_adeniyi <seunadeniyi06@gmail.com>
2022-09-14 04:32:46 -0700

    initial absolute value commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _abs - function prints number's absolute
+ * Return: Always 0.
+ * @int: parameter is passed
+ */
+int_abs(int)
+{
+	int x;
+
+	if (x > 0)
+	{
+		int p = x * (+1);
+
+		_putchar(p);
+	}
+	else if (x == 0)
+	{
+		int e = x * 0;
+
+		_putchar();
+	}
+	else
+	{
+		int n = x * (-1);
+
+		_putchar(n);
+	}
+
+	return (0);
+}
