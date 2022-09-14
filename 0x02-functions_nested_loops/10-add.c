1e2ab8511262fe7b11dcdaf0bf3e767f5caaae6b
seun_adeniyi <seunadeniyi06@gmail.com>
2022-09-14 07:07:19 -0700

    initila 10 commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - function prints number's absolute
+ * Return: the value
+ * @x: parameter is passed
+ * @y: parameter is passed
+ * Description: xy is an local parameter
+ */
+int add(int x, int y)
+{
+	int z;
+
+	z = x + y;
+	return (z);
+}
