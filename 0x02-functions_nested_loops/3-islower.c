42e73532ff585a41d65572e989ab7631b4d6a22f
seun_adeniyi <seunadeniyi06@gmail.com>
2022-09-14 02:17:19 -0700

    this is is commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _islower - function checks if character is lower case or not
+ * Return: 1 if true and 0 if false
+ * @c: parameter is passed
+ */
+int main(void)
+{
+int _islower(int c)
+{
+	return (c >= 97 && c <= 122);
+
+}
+}
