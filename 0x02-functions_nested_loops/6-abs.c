b1e6d46b0fc19751f197b4fa6a0f2d3ddfc67ce1
seun_adeniyi <seunadeniyi06@gmail.com>
2022-09-14 04:55:47 -0700

    changed the if combination for return config

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,22 +1,10 @@
 #include "main.h"
 /**
  * _abs - function prints number's absolute
- * Return: Always 0.
+ * Return: Not always 0
  * @int: parameter is passed
  */
 int _abs(int x)
 {
-	if (x > 0)
-	{
-		_putchar('%d', (x * (+1)));
-	}
-	else if (x == 0)
-	{
-		_putchar('%d', (x * 0));
-	}
-	else
-	{
-		_putchar('%d', (x * (-1)));
-	}
-	return (0);
+	return (x >= 0 ? n : n * -1);
 }
