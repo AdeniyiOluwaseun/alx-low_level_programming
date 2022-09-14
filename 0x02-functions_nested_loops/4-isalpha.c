d09f8837acfa9f8c8f6b263f31657be658b92dcb
seun_adeniyi <seunadeniyi06@gmail.com>
2022-09-14 02:35:27 -0700

    reject non letter xter

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,10 +1,13 @@
 #include "main.h"
 /**
- * _islower - function checks if character is lower case or not
+ * _isalpha - function checks if character is lower case or not
  * Return: 1 if true and 0 if false
  * @c: parameter is passed
  */
 int _isalpha(int c)
 {
-	return (c >= 97 && c <= 122 || c >= 65 && c <= 90);
+	if (c >= 97 && c <= 122 && c >= 65 && c <= 90);
+	{
+		return (c >= 97 && c <= 122 || c >= 65 && c <= 90);
+	}
 }
