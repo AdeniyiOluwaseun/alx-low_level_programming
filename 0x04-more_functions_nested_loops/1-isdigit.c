d496381a43e5b73706339679465a98bc6a3b7752
seun_adeniyi <seunadeniyi06@gmail.com>
2022-09-16 03:10:14 -0700

    initial commit for is digit function

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isdigit - function checks if character is lower case or not
+ * Return: 1 if true and 0 if false
+ * @c: parameter is passed
+ */
+int _isdigit(int c)
+{
+	return (c >= 48 && c <= 57);
+}
