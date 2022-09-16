0da919fb1aebb4e0f75c8d895b8378888d4538d9
seun_adeniyi <seunadeniyi06@gmail.com>
2022-09-16 15:51:04 -0700

    initial commit

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * more_numbers - Print numbers between 0 to 14 incl.
+ *
+ * Return: Void.
+ */
+void more_numbers(void)
+{
+	int i;
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (i = 0; i <= 14; i++;)
+		{
+			if (i >= 10)
+			{
+				_putchar(i / 10 + '0');
+			}
+				_putchar(i % 10 + '0');
+		}
+	_putchar('\n');
+	}
+}
+
