09ea50683875e9c2958e2eb9debb459c6b352726
seun_adeniyi <seunadeniyi06@gmail.com>
2022-09-16 03:03:40 -0700

    initial commit for _isupper funtion

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isupper - function checks if character is lower case or not
+ * Return: 1 if true and 0 if false
+ * @c: parameter is passed
+ */
+int _isupper(int c)
+{
+	return (c >= 65 && c <= 90);
+}
