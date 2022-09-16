cbe6816270a85eff3db472c209a668ff004337c1
seun_adeniyi <seunadeniyi06@gmail.com>
2022-09-16 03:19:40 -0700

    intial commit for mul function

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - function multiplies two integers
+ * Return: 0 Always
+ * @a: parameter is passed
+ * @b: second parameter passed
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
