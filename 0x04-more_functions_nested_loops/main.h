daa38a3397507bd0a48ca163f93d3e170dc8b908
seun_adeniyi <seunadeniyi06@gmail.com>
2022-09-17 07:08:05 -0700

    inti

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * main - header file for prototypes
+ * void protypes (void)
+ * int prototypes (int)
+ */
+
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -6,3 +15,4 @@ void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 
+#endif /* MAIN_H */
