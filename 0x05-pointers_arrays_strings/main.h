28052118563295f11cb221cb4bc352b95f637856
seun_adeniyi <seunadeniyi06@gmail.com>
2022-09-20 07:56:53 -0700

    initial project commit

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * main - header file for prototypes
+ * void protypes (void)
+ * int prototypes (int)
+ */
+
+int _putchar(char c);
+void reset_to_98(int *n);
+
+#endif /* MAIN_H */
