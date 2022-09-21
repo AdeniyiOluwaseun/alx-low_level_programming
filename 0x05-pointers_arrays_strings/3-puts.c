a719a8d99b7d4add4ff020471a7fae137a3550e1
seun_adeniyi <seunadeniyi06@gmail.com>
2022-09-20 20:01:54 -0700

    initial commit

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - prints string.
+ * @str: string type.
+ *
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	while (*str++)
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
+}
