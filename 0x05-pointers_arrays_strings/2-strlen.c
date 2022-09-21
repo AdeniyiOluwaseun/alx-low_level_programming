932e2c6e61ad2455b7e7b9ca3568799977ffa1bb
seun_adeniyi <seunadeniyi06@gmail.com>
2022-09-20 19:23:46 -0700

    initial commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strlen - Returns length a string.
+ * @str: string for length of.
+ *
+ * Return: length of @str.
+ */
+
+int _strlen(const char *str)
+{
+	int l = sizeof(str) - 1;
+
+	return (l);
+}
