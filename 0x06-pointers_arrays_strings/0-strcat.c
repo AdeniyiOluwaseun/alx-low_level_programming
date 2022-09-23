9897087c6307a60c9dc534bb966d8764827c1260
seun_adeniyi <seunadeniyi06@gmail.com>
2022-09-23 13:58:13 -0700

    intiates

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,7 +2,6 @@
 
 /**
  * _strcat - function that append string one to string two
- * _strlen - function that return the legnth of a string
  * @dest: first parameter
  * @src: second parameter
  * Return: dest
@@ -10,14 +9,32 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int len1 = _strlen(dest);
-	int len2 = _strlen(src);
+	int l, m, len1, len2, x;
 
-	int x;
+	l = 0;
+
+/*get the length of dest array*/
+	while (dest[l] != '\0')
+	{
+		l++;
+	}
+	len1 = l;
+
+/* get the length of src array*/
+	m = 0;
+
+	while (src[m] != '\0')
+	{
+		m++;
+	}
+	len2 = m;
+
+/* iterate through src and append it to dest*/
+	x = 0;
 
 	for (x = 0; x <= len2; x++)
 	{
-		dest[len1 + 1] = src[x];
+		dest[len1 + x] = src[x];
 	}
 	return (dest);
 }
