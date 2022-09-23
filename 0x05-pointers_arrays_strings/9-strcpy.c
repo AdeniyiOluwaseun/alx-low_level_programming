29fc654580a4c8165b8a62a7103447794626d621
seun_adeniyi <seunadeniyi06@gmail.com>
2022-09-23 15:51:27 -0700

    initial commit

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strcpy - copies one string to another
+ * @dest: First parameter.
+ * @src: Second paraeter.
+ *
+ * Return: first parameter
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int x;
+
+	for (x = 0; src[x] != '\0'; x++)
+	{
+		dest[x] = src[x];
+	}
+	return (dest);
+}
