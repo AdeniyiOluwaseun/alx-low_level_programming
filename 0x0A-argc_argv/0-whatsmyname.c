4809a914d091944475e43afe5e4a74716c8b6549
seun_adeniyi <seunadeniyi06@gmail.com>
2022-10-03 03:43:49 -0700

    initial commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - program that returns name of itself
+ * @argc: parameter that stores the count of all cmd input
+ * @argv: parameter that stores all cmd input data
+ * Return: 0 when successful
+ */
+
+int main(int argc, char *argv[])
+{
+	while (argc <= 1)
+	{
+	printf("%s\n", *argv);
+	argc++;
+	}
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,3 @@
+0x0A. C - argc, argv
+
+How to use arguments passed to your program
