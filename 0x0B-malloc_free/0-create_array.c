3c3e635bc34286d77b57ad6bacff7f0bd3dda5b2
seun_adeniyi <seunadeniyi06@gmail.com>
2022-10-04 21:42:24 -0700

    initial commit for all

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * create_array - a function that creates an array
+ * @size: firstt parameter int
+ * @c: second parameter char
+ * Return: pointer to the array, NULL if it fails
+ */
+
+
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int index;
+
+	if (size == 0)
+		return (NULL);
+
+	array = malloc(sizeof(char) * size);
+
+	if (array == NULL)
+		return (NULL);
+
+	for (index = 0; index < size; index++)
+		array[index] = c;
+
+	return (array);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,3 @@
+0x0B. C - malloc, free
+C memory allocation
+Difference between automatic and dynamic memory allocation in C
