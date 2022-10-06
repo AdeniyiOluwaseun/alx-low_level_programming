d2c75a6757172fcdc7ebfe26dfef540f29d6c564
seun_adeniyi <seunadeniyi06@gmail.com>
2022-10-05 21:43:24 -0700

    delete from strncat

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -10,33 +10,5 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int l, m, len1, len2, x;
 
-	l = 0;
-
-/*get the length of dest array*/
-	while (dest[l] != '\0')
-	{
-		l++;
-	}
-	len1 = l;
-
-/* get the length of src array*/
-	m = 0;
-
-	while (src[m] != '\0')
-	{
-		m++;
-	}
-	len2 = m;
-
-/* iterate through src and append it to dest*/
-
-	x = 0;
-
-	for (x = 0; x <= len2 && x < n; x++)
-	{
-		dest[len1 + x] = src[x];
-	}
-	return (dest);
 }
