47a081620bf969bba88cd7893a341a490eedeb61
seun_adeniyi <seunadeniyi06@gmail.com>
2022-10-11 19:04:55 -0700

    initial commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,16 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
 
-/**
- * file: dog.h
+/*
+ * File: dog.h
+ * Desc: Header file that defines a new type struct dog.
  */
 
 /**
- * struct: dog
- * descripition:
- * @age:
- * @name
- * @owner:mparameter
+ * struct dog - A new type describing a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
  */
 
 struct dog
