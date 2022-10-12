0814cb09e943b64645063d80bdc63e4dcf2b7165
seun_adeniyi <seunadeniyi06@gmail.com>
2022-10-11 18:54:52 -0700

    initial dog.h push

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * file: dog.h
+ */
+
+/**
+ * struct: dog
+ * descripition:
+ * @age:
+ * @name
+ * @owner:mparameter
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
