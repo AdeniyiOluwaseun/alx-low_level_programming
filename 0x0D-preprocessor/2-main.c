3d4c018f0370a7979f93baacfbac5ecad35eceac
seun_adeniyi <seunadeniyi06@gmail.com>
2022-10-11 17:36:15 -0700

    initial commit for 2-mIain.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - main funtion
+ * Description: program prints the name of it's source file
+ * Return: return 0 for successful
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
