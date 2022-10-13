af7fa48bb481affe60a360577dfe8bad393d731f
seun_adeniyi <seunadeniyi06@gmail.com>
2022-10-13 05:58:45 -0700

    initial coomit

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * function_pointers - header file for prototypes
+ * void protypes (void)
+ * 
+ */
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+
+#endif /* MAIN_H */
